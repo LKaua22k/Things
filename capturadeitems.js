648a61ea5e14e9991f675c720547e9cb86030739
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-21 11:33:10 -0300

    :tada:

--- capturadeitems.js
+++ capturadeitems.js
@@ -1,9 +1,10 @@
 let items = [];
 
+// se o intem for menor que 10 sera acrescentado +1
 for (let item = 0; item < 10; item++) {
     let itemName = prompt("Digite o item" + (item + 1))
     
-    items[item] = itemName
+    items[item] = itemName // Adicionando os intem nas lista
 }
 
 alert(items)
--- jogodaadivinhação.js
+++ jogodaadivinhação.js
@@ -1,10 +1,11 @@
 let P = prompt("1 A 10")
-const radom = Math.round(Math.random() * 10)
+const radom = Math.round(Math.random() * 10) //Gerando um numero aauto
 let x = 1
 
+//Estrutura de repetição 
 while( Number(P) != radom){
     P = prompt('Erro! tente novamente')
-    x++
+    x++ // A cada tentativa o x esta recebendo + 1
 }
 
 if(P < 2){
