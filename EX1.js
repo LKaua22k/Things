887b956b0f0909b10c0744b16add83afbb3e8d63
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-23 22:34:30 -0300

    :tada:

---
+++ EX1.js
@@ -0,0 +1,5 @@
+const P1 = Number(prompt("Digite o primeiro numero"))
+const P2 = Number(prompt("Digite o primeiro numero"))
+
+alert(`A soma dos dois números é : ${P1 + P2}`)
+alert(`A soma dos dois números é : ${P1 + P2}`)
