ee1f63096505556fe4ea2612fa2908b2583882fb
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-24 00:23:35 -0300

    :tada:

--- EX2.js
+++ EX2.js
@@ -20,19 +20,16 @@ function calc(nota1 , nota2){
     return (nota1 + nota2) / 2
 }
 
-function aprove(student){
+function printMedia(student){
     let result = calc(student.n1 , student.n2)
 
     if(result >= 7){
-        return `Parabens ${student.nome}! Você foi aprovado(a) no concurso`
+        return `A media do(a) aluno(a) ${student.nome} é de ${result}
+      Parabens ${student.nome}! Você foi aprovado(a) no concurso`
     } else{
-        return`Não foi dessa vez ${student.nome}! Tente Novamente`
+        return`A media do(a) aluno(a) ${student.nome} é de ${result}
+      Não foi dessa vez ${student.nome}! Tente Novamente`
     }
-}
-
-function printMedia(student){
-    return`A media do(a) aluno(a) ${student.nome}
-    ${aprove(student)}`
 }
 
 for (const student of students) {
