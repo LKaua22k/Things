61ad5d0c6e2cac219d9381bed7d7694df2d2ae87
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-23 23:38:34 -0300

    :tada:

--- EX1.js
+++ EX1.js
@@ -1,5 +1,22 @@
 const P1 = Number(prompt("Digite o primeiro numero"))
 const P2 = Number(prompt("Digite o primeiro numero"))
+const soma = P1 + P2
 
-alert(`A soma dos dois números é : ${P1 + P2}`)
-alert(`A soma dos dois números é : ${P1 + P2}`)
+alert(`A soma dos dois números é : ${soma}`)
+alert(`A subtração dos dois números é : ${P1 - P2}`)
+alert(`A multiplicação dos dois números; é : ${P1 * P2}`)
+alert(`A divisão dos dois números é : ${P1 / P2}`)
+alert(`O resto da divisão dos dois números é : ${P1 % P2}`)
+let dif = soma % 2
+
+if(dif == 0){
+  alert(`A soma dos numeros inseridos é par: ${soma}`)
+} else{
+  alert(`A soma dos numeros inseridos é impar: ${soma}`)
+}
+
+if(P1 == P2){
+  alert("Os numeros inseridos são iguais")
+} else{
+  alert("Os numeros inseridos são diferentes")
+}
